38612994cfe99d4ae290bc06f9c0917f2aecfad7
Werozel <khaibulaev.mn@gmail.com>
2020-04-20 12:01:54 +0300

    my info

--- config.py
+++ config.py
@@ -1,11 +1,9 @@
 
-""" Fill your info """
+db_username = "postgres"
+db_password = "4Vii64sh"
+db_name = "seenbot"
+db_host = "localhost"
+db_port = 5432
 
-db_username = 
-db_password = 
-db_name = 
-db_host = 
-db_port = 
-
-vk_secret = 
-vk_groupId = 
+vk_secret = "2682e7b4128fd4009ab1930546df7005a6c471b7b48b026085594c4bbf2475b383ea903f55999bdba24da"
+vk_groupId = 188481255
