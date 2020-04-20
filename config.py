8dd2bc66d437330ac92d203e6605067e6da1db78
Werozel <khaibulaev.mn@gmail.com>
2020-04-20 12:02:59 +0300

    Removed config

--- config.py
+++
@@ -1,9 +0,0 @@
-
-db_username = "postgres"
-db_password = "4Vii64sh"
-db_name = "seenbot"
-db_host = "localhost"
-db_port = 5432
-
-vk_secret = "2682e7b4128fd4009ab1930546df7005a6c471b7b48b026085594c4bbf2475b383ea903f55999bdba24da"
-vk_groupId = 188481255
