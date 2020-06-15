de6ee567bd51f46f6302abbbddfeadbab4d53c4a
Cactiw <Knyazev1514@gmail.com>
2020-06-16 01:38:12 +0300

    Fixed sorting algorithm

--- src/handlers/karma/show_karma_handler.py
+++ src/handlers/karma/show_karma_handler.py
@@ -11,9 +11,7 @@ def check_func(msg):
 def process_func(msg):
     users = session.query(User).order_by().all()
     stats: list = list(map(lambda x: x.show_stat(), 
-                       sorted(users, key=lambda user: (user.ups / user.downs if user.all_pics > 0 else 0,
-                                                       -user.downs/user.all_pics if user.all_pics > 0 else 0,
-                                                       user.ups/user.all_pics if user.all_pics > 0 else 1000, ))))
+        sorted(users, key=lambda user: (user.ups / user.downs) * (user.ups / user.all_pics) / (user.downs / user.all_pics) if user.downs > 0 and user.all_pics > 0 else 0)))
     stat_str = '\n'.join(stats)
     peer_id = msg.get('peer_id')
     api.messages.send(peer_id=peer_id,
