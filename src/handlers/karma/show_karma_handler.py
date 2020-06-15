f74fbedc815ea622bb36e8958336f026d2107f5f
Werozel <khaibulaev.mn@gmail.com>
2020-06-16 01:16:49 +0300

    Changed sorting algorithm

--- src/handlers/karma/show_karma_handler.py
+++ src/handlers/karma/show_karma_handler.py
@@ -1,7 +1,7 @@
 from libs.Handler import Handler
 from globals import api, get_rand, session
-from sqlalchemy import desc
 from libs.User import User
+
 
 def check_func(msg):
     text = msg.get('text')
@@ -11,12 +11,14 @@ def check_func(msg):
 def process_func(msg):
     users = session.query(User).order_by().all()
     stats: list = list(map(lambda x: x.show_stat(), 
-                       sorted(users, key=lambda user: (user.downs / user.all_pics if user.all_pics > 0 else 1000, 
-                                                       -user.ups/user.all_pics if user.all_pics > 0 else 0, ))))
+                       sorted(users, key=lambda user: (user.ups / user.downs if user.all_pics > 0 else 0,
+                                                       -user.downs/user.all_pics if user.all_pics > 0 else 0,
+                                                       user.ups/user.all_pics if user.all_pics > 0 else 1000, ))))
     stat_str = '\n'.join(stats)
     peer_id = msg.get('peer_id')
-    api.messages.send(peer_id = peer_id,
-                     message=stat_str,
-                     random_id=get_rand())
+    api.messages.send(peer_id=peer_id,
+                      message=stat_str,
+                      random_id=get_rand())
+
 
 handler = Handler(check_func, process_func)
