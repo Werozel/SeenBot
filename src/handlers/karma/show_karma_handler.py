7f5ddd0aafb1e45c34df18ca9e5c3fae6364bcfc
Werozel <khaibulaev.mn@gmail.com>
2020-11-05 20:27:01 +0300

    Changed sorting key

--- src/handlers/karma/show_karma_handler.py
+++ src/handlers/karma/show_karma_handler.py
@@ -12,12 +12,13 @@ def check_func(msg):
 def process_func(msg):
     users = session.query(User).order_by().all()
     stats: list = list(map(lambda x: x.show_stat(),
-                           sorted(users, key=lambda user: (user.ups / user.downs) * (
-                                       user.ups + user.downs) * user.all_pics if user.downs > 0 else 0)))
+                           sorted(users,
+                                  key=lambda user: (user.ups / user.downs) * user.all_pics if user.downs > 0 else 0)
+                           ))
     stat_str = '\n'.join(stats)
     peer_id = msg.get('peer_id')
     api.messages.send(peer_id=peer_id,
-                      message=stat_str,
+                      message=stat_str + f" ",
                       random_id=get_rand())
 
 
