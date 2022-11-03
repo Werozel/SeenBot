c23046838a085da32b3333968183b4ea2464eb06
Werozel <khaibulaev.mn@gmail.com>
2022-11-03 20:16:49 +0300

    completed populate

--- migration/populate_from_csvs.py
+++ migration/populate_from_csvs.py
@@ -3,11 +3,12 @@ import pandas as pd
 from datetime import datetime
 
 # Necessary imports
-from libs.Phrase import Phrase
 from libs.Achievement import Achievement
+from libs.Phrase import Phrase
 from libs.DownloadedPic import DownloadedPic
 from libs.RawLink import RawLink
 from libs.PicMessage import PicMessage
+from libs.PictureSize import PictureSize
 from libs.Picture import Picture
 from libs.User import User
 
@@ -51,23 +52,79 @@ def populate_users():
 
 
 def populate_pictures():
-    pass
+    df: pd.DataFrame = read_csv("pictures.csv")
+    for _, row in df.iterrows():
+        picture = Picture(
+            id=int(row["id"]),
+            ups=int(row["ups"]),
+            downs=int(row["downs"]),
+            bads=int(row["bads"]),
+            user_id=int(row["user_id"]),
+            owner_id=int(row["owner_id"]),
+            access_key=row["access_key"],
+            add_time=datetime_from_string(row["add_time"])
+        )
+        session.add(picture)
+    session.commit()
 
 
 def populate_sizes():
-    pass
+    df: pd.DataFrame = read_csv("sizes.csv")
+    for _, row in df.iterrows():
+        pic_size = PictureSize(
+            id=int(row["id"]),
+            pic_id=int(row["pic_id"]),
+            size=row["size"],
+            link=row["link"],
+            add_time=datetime_from_string(row["add_time"])
+        )
+        session.add(pic_size)
+    session.commit()
 
 
 def populate_messages():
-    pass
+    df: pd.DataFrame = read_csv("messages.csv")
+    for _, row in df.iterrows():
+        msg = PicMessage(
+            id=int(row["id"]),
+            user_id=int(row["user_id"]),
+            pic_id=int(row["pic_id"]),
+            time=datetime_from_string(row["time"]),
+            text=row["text"]
+        )
+        session.add(msg)
+    session.commit()
 
 
 def populate_raw_links():
-    pass
+    df: pd.DataFrame = read_csv("raw_links.csv")
+    for _, row in df.iterrows():
+        raw_link = RawLink(
+            id=int(row["id"]),
+            type=row["type"],
+            owner_id=int(row["owner_id"]),
+            access_key=row["access_key"],
+            track_code=row["track_row"],
+            url=row["url"],
+            user_id=int(row["user_id"]),
+            add_time=datetime_from_string("add_time")
+        )
+        session.add(raw_link)
+    session.commit()
 
 
 def populate_downloaded_pics():
-    pass
+    df: pd.DataFrame = read_csv("downloaded_pics.csv")
+    for _, row in df.iterrows():
+        downloaded_pic = DownloadedPic(
+            id=int(row["id"]),
+            picture_id=int(row["picture_id"]),
+            album_id=row["album_id"],
+            owner_id=int(row["owner_id"]),
+            access_key=row["access_key"]
+        )
+        session.add(downloaded_pic)
+    session.commit()
 
 
 def main():
