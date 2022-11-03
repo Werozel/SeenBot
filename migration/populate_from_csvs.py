1dfd41d8f45edfa53a0514392e28a7e7619f12ff
Werozel <khaibulaev.mn@gmail.com>
2022-11-03 18:11:52 +0300

    completed populate user

--- migration/populate_from_csvs.py
+++ migration/populate_from_csvs.py
@@ -1,10 +1,14 @@
-from libs.Phrase import Phrase
 from globals import session
 import pandas as pd
 from datetime import datetime
 
 # Necessary imports
+from libs.Phrase import Phrase
+from libs.Achievement import Achievement
+from libs.DownloadedPic import DownloadedPic
 from libs.RawLink import RawLink
+from libs.PicMessage import PicMessage
+from libs.Picture import Picture
 from libs.User import User
 
 
@@ -29,7 +33,50 @@ def populate_phrases():
     session.commit()
 
 
+def populate_users():
+    df: pd.DataFrame = read_csv("users.csv")
+    for _, row in df.iterrows():
+        user = User(
+            id=int(row['id']),
+            first_name=row['first_name'],
+            last_name=row['last_name'],
+            ups=int(row['ups']),
+            downs=int(row['downs']),
+            bads=int(row['bads']),
+            all_pics=int(row['all_pics']),
+            add_time=datetime_from_string(row['add_time'])
+        )
+        session.add(user)
+    session.commit()
+
+
+def populate_pictures():
+    pass
+
+
+def populate_sizes():
+    pass
+
+
+def populate_messages():
+    pass
+
+
+def populate_raw_links():
+    pass
+
+
+def populate_downloaded_pics():
+    pass
+
+
 def main():
+    populate_users()
+    populate_pictures()
+    populate_sizes()
+    populate_messages()
+    populate_raw_links()
+    populate_downloaded_pics()
     populate_phrases()
 
 
